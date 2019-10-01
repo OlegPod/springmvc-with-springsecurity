e59be04336c55ff4a4e41fda69b579d587c40ac7
OlegPod <kepacuh@gmail.com>
2019-10-02 00:44:49 +0300

    Implemented creating of a new Product. #1

--- src/main/java/com/olehpodolin/springmvcwithsecurity/controllers/ProductController.java
+++ src/main/java/com/olehpodolin/springmvcwithsecurity/controllers/ProductController.java
@@ -1,11 +1,13 @@
 package com.olehpodolin.springmvcwithsecurity.controllers;
 
+import com.olehpodolin.springmvcwithsecurity.domain.Product;
 import com.olehpodolin.springmvcwithsecurity.services.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class ProductController {
@@ -16,7 +18,7 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @RequestMapping("/products")
+    @GetMapping("/products")
     public String listAllProducts(Model model) {
 
         model.addAttribute("products", productService.listAllProducts());
@@ -30,6 +32,22 @@ public class ProductController {
         model.addAttribute("product", productService.getProductById(Long.valueOf(id)));
 
         return "showproduct";
+    }
+
+    @GetMapping("/products/new")
+    public String createNewProduct(Model model) {
+
+        model.addAttribute("product", new Product());
+
+        return "productform";
+    }
+
+    @PostMapping("/product")
+    public String saveOrUpdateProduct(Product product) {
+
+        Product savedProduct = productService.save(product);
+
+        return "redirect:/products/" + savedProduct.getId() + "/show";
     }
 
 }
